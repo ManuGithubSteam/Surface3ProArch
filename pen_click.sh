d8d2034bf215983992d8938b4691af7b2fdf0068
Manuel <manuelsoukup@googlemail.com>
2018-02-05 22:32:13 +0100

    fixed but with mac adress
    should be more reliable

--- pen_click.sh
+++ pen_click.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # the mac adress of your pen
-blueMAC="EB:DD:F0:62:CF:6B"
+#blueMAC="EB:DD:F0:62:CF:6B"
 # command to execute
 command="xournalpp"
 
@@ -15,10 +15,11 @@ while read line
 do
 #echo "$line"
  
-	if [[ $line = *"bluetoothd: No cache for $blueMAC"* ]];
+	if [[ $line = *"Reason: Connection Timeout"* ]];
 	then
 		# echo "inside!"
-		# echo "$line"
+		#
+		echo "$line"
 		# test if running
 		ps -ef | grep "$command" | grep -v grep
 		if [ $? -eq 1 ]
